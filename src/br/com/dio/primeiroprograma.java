69000f3f7728b594fc889ad7760a2f646403d91b
cainã <cainanmarcos1234@gmail.com>
2023-03-07 13:13:47 -0300

    first commit

---
+++ src/br/com/dio/primeiroprograma.java
@@ -0,0 +1,12 @@
+package br.com.dio;
+
+
+import br.com.dio.model.gato;
+
+public class primeiroprograma {
+    public static void main(String[] args) {
+        gato gato = new gato();
+
+        System.out.println(gato);
+    }
+}
